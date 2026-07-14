2ac1013c8ace11c4b627ed2adc3f2b30fdff0932
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2026-07-14 23:38:28 +0900

    Add files via upload

--- demo/member.js
+++ demo/member.js
@@ -1,14 +1,15 @@
-document.getElementById("detail").addEventListener("click",(ele)=>{
-    //var top = document.getElementById("detail1").offset().top;
-    var z = document.getElementById("detail").getBoundingClientRect();
+
+document.getElementById("close").addEventListener("click",()=>{
+    document.getElementById("pop").style.display = "none";
+})
+
+function pop(number){
+    var posi = "detail"+number;
+    var z = document.getElementById(posi).getBoundingClientRect();
     console.log(z.left-screen.width*0.14);
     document.getElementById("pop").style.display = "block";
     document.getElementById("pop").style.opacity = "1";
     document.getElementById("pop").style.position = "absolute";
     document.getElementById("pop").style.left = z.left-screen.width*0.14+"px";
     document.getElementById("pop").style.top = z.top-screen.height*0.34+"px";
-})
-
-document.getElementById("close").addEventListener("click",()=>{
-    document.getElementById("pop").style.display = "none";
-})
+}
